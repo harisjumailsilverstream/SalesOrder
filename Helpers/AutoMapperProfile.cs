a252c4420a40568923580056069527f525751128
Haris Jumail <58248783+harisjumail@users.noreply.github.com>
2025-01-10 19:02:14 +0700

    first init
    first init

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using SalesOrder.DTOs;
+using SalesOrder.Entities;
+
+namespace SalesOrder.Helpers;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        // Existing mappings
+        CreateMap<OrderDto, SoOrder>()
+            .ForMember(dest => dest.Customer, opt => opt.Ignore())
+            .ForMember(dest => dest.Items, opt => opt.Ignore()) // Items handled manually
+            .ReverseMap()
+            .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Customer.CustomerName))
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
+
+        CreateMap<ItemDto, SoItem>()
+            .ForMember(dest => dest.SoOrderId, opt => opt.Ignore()) // Set manually
+            .ReverseMap()
+            .ForMember(dest => dest.SoItemId, opt => opt.MapFrom(src => src.SoItemId)); // Ensure mapping
+
+        CreateMap<CustomerDto, ComCustomer>().ReverseMap();
+
+        CreateMap<OrderDto, OrderDto>(); 
+    }
+}
